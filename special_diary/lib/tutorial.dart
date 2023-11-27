278de6156732e95b9e91a84c220245c528065404
seola <132555534+parkseola89@users.noreply.github.com>
2023-11-27 17:10:40 +0900

    튜토리얼수정, 이미지 추추가

--- special_diary/lib/tutorial.dart
+++ special_diary/lib/tutorial.dart
@@ -31,16 +31,25 @@ class _TutorialState extends State<Tutorial> {
         // backgroundColor: Theme.of(context).colorScheme.primaryContainer,
         foregroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
       ),
-      body: SingleChildScrollView(
-        child: Center(
+      body: Center(
+        child: SingleChildScrollView(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+        
             children: [
               Text('TUTORIAL',),
               SizedBox(height: 20,),
               Text('1. Home화면'),
-      
+                Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+              Image.asset('images/eventfirst.png',width: 150,height: 300,),
+              Image.asset('images/eventsecond.png',width: 150,height: 300,),
+                ],
+              ),
               
               
+              SizedBox(height: 20,),
               Text('2. Event화면 '),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -51,11 +60,13 @@ class _TutorialState extends State<Tutorial> {
               ),
               Text('기록할 날짜를 지정 후 +버튼 누르면 입력창으로 이동합니다.'),
               Text('2. Event입력'),
+              Image.asset('images/eventinsert.png',width: 150,height: 300,),
+              Text('날씨선택, 제목, 내용, 사진을 선택하여 입력 버튼을 누를 시 기록한 내용이 저장됩니다.'),
               Text('3. Memo입력'),
-      
+              
               
             ],
-      
+              
           ),
         ),
       ),
