2174cacea58366b8d4c98cd8e98a549f0a623cc3
seola <132555534+parkseola89@users.noreply.github.com>
2023-12-08 18:22:16 +0900

    이미지 삭제 후 직접 table 만듦

--- special_diary/lib/service.dart
+++ special_diary/lib/service.dart
@@ -6,6 +6,58 @@ class TermsOfService extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+        TableRow _buildColoredTableRow(List<String> cells, Color color) {
+    return TableRow(
+      children: cells
+          .map((cell) => Container(
+                color: color,
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(5.0),
+                child: Text(
+                  cell,
+                  style: TextStyle(
+                    color: Colors.black, // Set text color to black
+                  ),
+                ),
+              ))
+          .toList(),
+    );
+  }
+
+      TableRow _buildColoredTableOneRow(List<String> cells) {
+    return TableRow(
+      children: cells
+          .map((cell) => Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(5.0),
+                child: Text(
+                  cell,
+                  style: TextStyle(
+                    color: Colors.black, // Set text color to black
+                  ),
+                ),
+              ))
+          .toList(),
+    );
+  }
+      TableRow _buildColoredTableTwoRow(List<String> cells) {
+    return TableRow(
+      children: cells
+          .map((cell) => Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(5.0),
+                child: Text(
+                  cell,
+                  style: TextStyle(
+                    color: Colors.black, // Set text color to black
+                  ),
+                ),
+              ))
+          .toList(),
+    );
+  }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 236, 234, 234),
@@ -34,7 +86,7 @@ class TermsOfService extends StatelessWidget {
                 Text(
                     '❮My Special Diary❯는 다이어리 서비스를 제공함에 있어 「개인정보 보호법」 및 관계 법령이 정한 바를 준수하여, 적법하게 개인정보를 처리하고 안전하게 관리하고 있습니다. 이에 「개인정보 보호법」 제 30조에 따라 정보주체에게 개인정보 처에 관한 절차 및 기준을 안내하고, 이용자의 개인정보를 보호하고 이를 관리하기 위해 다음과 같은 처리방침을 둡니다'),
                 SizedBox(
-                  height: 25,
+                  height: 30,
                 ),
                 Text(
                   '개인정보의 처리목적',
@@ -47,7 +99,7 @@ class TermsOfService extends StatelessWidget {
                 Text('     • 고충처리 목적'),
                 Text('        각종 고지∙통지, 고충처리 목적으로 개인정보를 처리합니다. '),
                 SizedBox(
-                  height: 20,
+                  height: 30,
                 ),
                 Text(
                   '개인정보 처리 및 보유 기간',
@@ -60,81 +112,123 @@ class TermsOfService extends StatelessWidget {
                 Text('        • 이용자가 작성한 글, 사진 : 이용자가 삭제 요청 시까지'),
                 Text('        • <예외 사유>시에는 <보유기간>까지'),
                 SizedBox(
-                  height: 20,
+                  height: 30,
                 ),
                 Text(
-                  '제3조 개인정보의 제3자 제공',
+                  '처리하는 개인정보의 항목',
                   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                 ),
                 Text(
-                    '   3.1 My Special Diary는 원칙적으로 이용자의 동의 없이 개인정보를 외부에 제공하지 \n         않습니다. 다만, 다음의 경우에는 예외로 합니다.'),
-                Text('      • 이용자가 사전에 동의한 경우'),
-                DataTable(
-                  columns: <DataColumn>[
-                    DataColumn(label: Text('서비스')),
-                    DataColumn(label: Text('수집목적')),
-                    DataColumn(label: Text('수집항목')),
-                    DataColumn(label: Text('보유 및 \n이용기간')),
-                  ],
-                  rows: <DataRow>[
-                    DataRow(cells: <DataCell>[
-                      DataCell(Text('글 작성')),
-                      DataCell(Text('서비스 \n 제공')),
-                      DataCell(Text('이용자 사진')),
-                      DataCell(Text('이용자의 \n삭제 요청시까지')),
-                    ]),
-                    DataRow(cells: <DataCell>[
-                      DataCell(Text('관리')),
-                      DataCell(Text('고충처리 \n 목적')),
-                      DataCell(Text('이메일주소')),
-                      DataCell(Text('서비스 \n공급완료시까지')),
-                    ]),
-                  ],
-                  dataRowMaxHeight: 100,
-                  dataRowMinHeight: 50,
-                ),
-                Text('      • 법령의 규정에 따라 제공이 필요한 경우'),
+                    '  ❮My Special Diary❯는 다음의 개인정보 항목을 처리하고 있습니다.'),
+
+                    SizedBox(height: 5,),
+
+                  Table(
+                      border: TableBorder.all(),
+                      defaultVerticalAlignment:TableCellVerticalAlignment.middle,
+                      defaultColumnWidth: FixedColumnWidth(100.0),
+                      children: 
+                        <TableRow>[
+                          _buildColoredTableRow(['서비스', '수집 목적', '수집 항목', '보유 및 이용시간'], Colors.grey[300]!),
+                          _buildColoredTableOneRow(['글 작성', '서비스 제공', '이용자 사진', '이용자의 삭제 요청 시까지']),
+                          _buildColoredTableTwoRow(['회원 관리', '고충처리 목적', '이메일 주소', '서비스 공급 완료 시까지']),  
+                        ]
+                      
+                    ),
+
+
+
+
+
+
+
                 SizedBox(
-                  height: 20,
+                  height: 30,
                 ),
                 Text(
-                  '제4조 개인정보의 보유 및 파기',
+                  '개인정보의 파기 절차 및 방법에 관한 사항',
                   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                 ),
-                Text(
-                    '   4.1 회사는 이용자의 개인정보를 서비스 제공을 위한 기간 동안 보유하며, \n         목적이 달성된 경우 지체 없이 파기합니다.'),
+                Text('   ❮My Special Diary❯는 개인정보 보유기간의 경과, 처리목적 달성 등 개인정보가 불필요하게 되었을 때에는 지체없이 해당 개인정보를 파기합니다. 정보주체로부터 동의받은 개인정보 보유기간이 경과하거나 처리목적이 달성되었음에도 불구하고 다른 법령에 따라 개인정보를 계속 보존하여야 하는 경우에는, 해당 개인정보를 별도의 데이터 베이스(DB)로 옮기거나 보관장소를 달리하여 보존합니다.'),
+                
+                Text(' 개인정보 파기의 절차 및 방법은 다음과 같습니다.'),
+                Text(' 1. 파기절차'),
+                Text(' ❮My Special Diary❯는 파기 사유가 발생한 개인정보를 선정하고, ❮My Special Diary❯의 개인 정보 보호책임자의 승인을 받아 개인정보를 파기합니다.'),
+                Text(' 2. 파기방법'),
+                Text(' ❮My Special Diary❯는 전자적 파일 형태로 기록•저장된 개인정보는 기록을 재생할 수 없도록 파기하며, 종이 문서에 기록•저장된 개인정보는 분쇄기로 분쇄하거나 소각하여 파기합니다'),
+  
+
                 SizedBox(
-                  height: 20,
+                  height: 30,
                 ),
                 Text(
-                  '제5조 이용자의 권리와 의무',
+                  '정보주체와 법정대리인의 권리•의무 및 행사방법',
                   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                 ),
-                Text('   5.1 이용자는 언제든지 자신의 개인정보를 열람하고 수정할 수 있습니다.'),
-                Text('   5.2 이용자는 개인정보의 오류에 대한 정정을 요청할 수 있습니다.'),
-                Text('   5.3 이용자는 개인정보의 수집, 이용, 제공에 대한 동의 철회를 할 수 있습 \n  니다.'),
+                Text('① 정보주체는 ❮My Special Diary❯에 대해 언제든지 개인정보 열람•정정·삭제•처리정지 요구 등의 권리를 행사할 수 있습니다.'),
+                Text('※ 만 14세 미만 아동에 관한 개인정보의 열람등 요구는 법정대리인이 직접 해야 하며, 만 14세 이상의 미성년자인 정보주체는 정보주체의 개인정보에 관하여 미성년자 본인이 권리를 행사하거나 법정대리인을 통하여 권리를 행사할 수도 있습니다.'),
+                Text('② 권리 행사는 ❮My Special Diary❯에 대해 「개인정보 보호법」 시행령 제41조 제1항에 따라 서면, 전자우편, 모사전송(FAX) 등을 통하여 하실 수 있으며, ❮My Special Diary❯는 이에 대해 지체없이 조치하겠습니다.'),
+                Text('③ 권리 행사는 정보주체의 법정대리인이나 위임을 받은 자 등 대리인을 통하여 하실 수도 있습니다.'),
+                Text('이 경우 "개인정보 처리 방법에 관한 고시(제2020-7호)" 별지 제11호 서식에 따른 위임장을 제출하셔야 합니다.'),
+                Text('④ 개인정보 열람 및 처리정지 요구는 「개인정보 보호법」 제35조 제4항, 제37조 제2항에 의하여 정보주체의 권리가 제한 될 수 있습니다.'),
+                Text('⑤ 개인정보의 정정 및 삭제 요구는 다른 법령에서 그 개인정보가 수집 대상으로 명시되어 있는 경우에는 그 삭제를 요구할 수 없습니다.'),
+                Text('이 경우 "개인정보 처리 방법에 관한 고시(제2020-7호)" 별지 제11호 서식에 따른 위임장을 제출하셔야 합니다.'),
+                Text(' ❮My Special Diary❯는 정보주체 권리에 따른 열람의 요구, 정정•삭제의 요구, 처리정지의 요구 시 열람 등 요구를 한 자가 본인이거나 정당한 대리인인지를 확인합니다.'),
+
                 SizedBox(
-                  height: 20,
+                  height: 30,
                 ),
                 Text(
-                  '제6조 개인정보보호',
+                  '개인정보의 안전성 확보조치',
                   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                 ),
-                Text(
-                    '회사는 회원의 개인정보를 본인의 동의 없이 타인에게 제공하지 않으며, \n 개인정보보호에 관한 법령에 따라 안전하게 관리합니다.'),
+                Text(' ❮My Special Diary❯는 개인정보의 안전성 확보를 위해 다음과 같은 조치를 취하고 있습니다.'),
+                Text('1. 관리적 조치 : 내부관리계획 수립• 시행'),
+                Text('2. 기술적 조치 : 개인정보처리시스템 등의 접근권한 관리, 보안프로그램 설치 및 갱신'),
                 SizedBox(
-                  height: 20,
+                  height: 30,
                 ),
                 Text(
-                  '제7조 개인정보 처리방침 변경',
+                  '개인정보 자동 수집 장치의 설치 운영 및 거부에 관한 사항',
                   style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                 ),
-                Text(
-                    '   7.1 이 개인정보 처리방침은 법령, 정책 또는 보안기술의 변경에 따라 \n       변경될 수 있습니다. 변경 시 본 페이지를 통해 공지합니다.'),
+                Text(' ❮My Special Diary❯는 모바일 앱에서 온라인 맞춤형 광고를 위하여 광고식별자를 수집•이용 합니다. 정보주체는 모바일 단말기의 설정 변경을 통해 앱의 맞춤형 광고를 차단·허용할 수 있습니다.'),
+                Text('(1) (안드로이드) ① 설정 - ② 개인정보보호- ③ 광고 - ③ 광고 ID 재설정 또는 광고ID 삭제'),
+                Text('(2) (아이폰) ① 설정 - ② 개인정보보호- ③ 추적 - ④ 앱이 추적을 요청하도록 허용 끔'),
+                Text('※ 모바일 OS 버전에 따라 메뉴 및 방법이 다소 상이할 수 있습니다.'),
                 SizedBox(
-                  height: 50,
+                  height: 30,
                 ),
-                Text('     본 개인정보 처리방침은 2023년 12월 XX일부터 시행됩니다.'),
+                Text(
+                  '개인정보 보호책임자',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+                Text('① ❮My Special Diary❯는 개인정보 처리에 관한 업무를 총괄해서 책임지고, 개인정보 처리와 관련한 정보주체의 불만처리 및 피해구제 등을 위하여 아래와 같이 개인정보 보호책임자를 지정하고 있습니다.'),
+                Text('• 개인정보 보호책임자'),
+                Text('    성명: 박설아'),
+                Text('    연락처: seolaox89@gmail.com'),
+                Text('② 정보주체는 ❮My Special Diary❯의 서비스를 이용하시면서 발생한 모든 개인정보보호 관련 문의, 불만처리, 피해구제 등에 관한 사항을 개인정보 보호책임자 및 담당부서로 문의할 수 있습니다. ❮My Special Diary❯는 정보주체의 문의에 대해 지체없이 답변 및 처리해드릴 것입니다.'),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  '권익침해 구제방법',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+                Text('① 정보주체는 개인정보침해로 인한 구제를 받기 위하여 개인정보분쟁조정위원회, 한국인터넷진흥원 개인정보침해신고센터 등에 분쟁해결이나 상담 등을 신청할 수 있습니다. 이 밖에 기타 개인정보침해의 신고, 상담에 대하여는 아래의 기관에 문의하시기 바랍니다.'),
+                Text('1. 개인정보분쟁조정위원회 : (국번없이) 1833-6972 (www.kopico.go.kr)'),
+                Text('2. 개인정보침해신고센터 : (국번없이) 118 (privacy.kisa.or.kr)'),
+                Text('3. 대검찰청 : (국번없이) 1301 (www.spo.go.kr)'),
+                Text('4. 경찰청 : (국번없이) 182 (ecrm.cyber.go.kr)'),
+                Text('② 「개인정보 보호법」 제35조(개인정보의 열람), 제36조(개인정보의 정정·삭제), 제37조(개인정보의 처리정지 등)의 규정에 의한 요구에 대 하여 공공기관의 장이 행한 처분 또는 부작위로 인하여 권리 또는 이익의 침해를 받은 자는 행정심판법이 정하는 바에 따라 행정심판을 청구할 수 있습니다.'),
+                SizedBox(
+                  height: 30,
+                ),
+                  Text(
+                  '개인정보 처리방침의 변경',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+                Text('본 개인정보 처리방침은 2023년 12월 10일부터 시행됩니다.'),
                 SizedBox(
                   height: 60,
                 ),
--- special_diary/lib/tutorial.dart
+++ special_diary/lib/tutorial.dart
@@ -16,8 +16,8 @@ class _TutorialState extends State<Tutorial> {
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
         toolbarHeight: 65,
-        title: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 0, 45, 0),
+        title: const Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 45, 0),
           child: AppbarTitle(),
         ),
         flexibleSpace: Container(
@@ -37,32 +37,32 @@ class _TutorialState extends State<Tutorial> {
             mainAxisAlignment: MainAxisAlignment.center,
         
             children: [
-              SizedBox(height: 20,),
-              Text('- TUTORIAL -',style: TextStyle(fontSize: 21,fontWeight: FontWeight.w900),),
+              const SizedBox(height: 20,),
+              const Text('- TUTORIAL -',style: TextStyle(fontSize: 21,fontWeight: FontWeight.w900),),
                             SizedBox(height: 20,),
-              Text('1. Date 선택 ',style: TextStyle(fontSize: 19,fontWeight: FontWeight.w700),),
+              const Text('1. Date 선택 ',style: TextStyle(fontSize: 19,fontWeight: FontWeight.w700),),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
               Image.asset('images/eventpage.png',width: 150,height: 300,),
                 ],
               ),
-              SizedBox(height: 10,),
-              Text('기록할 날짜를 지정 후 +버튼을 누르면 입력창으로 이동합니다.',),
-              SizedBox(height: 60,),
-              Text('2. Event 입력',style: TextStyle(fontSize: 19,fontWeight: FontWeight.w700),),
+              const SizedBox(height: 10,),
+              const Text('기록할 날짜를 지정 후 +버튼을 누르면 입력창으로 이동합니다.',),
+              const SizedBox(height: 60,),
+              const Text('2. Event 입력',style: TextStyle(fontSize: 19,fontWeight: FontWeight.w700),),
               Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Image.asset('images/eventinsert.png',width: 150,height: 300,),
                 ],
               ),
-                  SizedBox(height: 15,),
-              Text('날씨, 제목, 내용, 사진을 선택하여',),
-              Text('버튼을 누를 시 기록한 내용이 저장됩니다.',),
+              const SizedBox(height: 15,),
+              const Text('날씨, 제목, 내용, 사진을 선택하여',),
+              const Text('버튼을 누를 시 기록한 내용이 저장됩니다.',),
 
-              SizedBox(height: 60,),
-              Text('3. Home 화면',style: TextStyle(fontSize: 19,fontWeight: FontWeight.w700),),
+              const SizedBox(height: 60,),
+              const Text('3. Home 화면',style: TextStyle(fontSize: 19,fontWeight: FontWeight.w700),),
                 Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -71,11 +71,11 @@ class _TutorialState extends State<Tutorial> {
               Image.asset('images/eventdetail.png',width: 130,height: 270,),
                 ],
               ),
-              SizedBox(height: 10,),
-              Text('앱을 실행할 때 구성되는 첫 화면입니다.'),
-              Text('입력한 내용을 검색하면 키워드 검색으로 해당 기록물만 띄워줍니다.'),
-              Text('기록물을 클릭 시 입력한 내용을 자세히 볼 수 있습니다.'),
-              SizedBox(height: 50,),
+              const SizedBox(height: 10,),
+              const Text('앱을 실행할 때 구성되는 첫 화면입니다.'),
+              const Text('입력한 내용을 검색하면 키워드 검색으로 해당 기록물만 띄워줍니다.'),
+              const Text('기록물을 클릭 시 입력한 내용을 자세히 볼 수 있습니다.'),
+              const SizedBox(height: 50,),
                 Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -83,10 +83,10 @@ class _TutorialState extends State<Tutorial> {
               Image.asset('images/datepicker.png',width: 150,height: 300,),
                 ],
               ),
-              SizedBox(height: 10,),
-              Text('해당 카드를 오른쪽으로 밀고 edit를 클릭 시 수정 페이지로 이동합니다.'),
-              Text('날짜,날씨,제목,내용,사진등을 변경할 수 있습니다.'),
-              SizedBox(height: 50,),
+              const SizedBox(height: 10,),
+              const Text('해당 카드를 오른쪽으로 밀고 edit를 클릭 시 수정 페이지로 이동합니다.'),
+              const Text('날짜,날씨,제목,내용,사진등을 변경할 수 있습니다.'),
+              const SizedBox(height: 50,),
                 Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -94,11 +94,11 @@ class _TutorialState extends State<Tutorial> {
               Image.asset('images/deletesheet.png',width: 150,height: 300,),
                 ],
               ),
-              SizedBox(height: 10,),
-              Text('해당 카드를 왼쪽으로 밀고 delete를 클릭 시 삭제할 수 있습니다.'),
-              SizedBox(height: 60,),
+              const SizedBox(height: 10,),
+              const Text('해당 카드를 왼쪽으로 밀고 delete를 클릭 시 삭제할 수 있습니다.'),
+              const SizedBox(height: 60,),
               
-              Text('4. Memo 입력',style: TextStyle(fontSize: 19,fontWeight: FontWeight.w700),),
+              const Text('4. Memo 입력',style: TextStyle(fontSize: 19,fontWeight: FontWeight.w700),),
                 Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -107,12 +107,12 @@ class _TutorialState extends State<Tutorial> {
               Image.asset('images/memodelete.png',width: 130,height: 270,),
                 ],
               ),
-              SizedBox(height: 10,),
-              Text('+버튼 누르면 입력창을 띄웁니다.'),
-              Text('해당 카드를 클릭하거나 오른쪽으로 밀면 수정,'),
-              Text('왼쪽으로 밀면 삭제할 수 있습니다.'),
-              SizedBox(height: 50,),
-              Text('5. Theme 변경',style: TextStyle(fontSize: 19,fontWeight: FontWeight.w700),),
+              const SizedBox(height: 10,),
+              const Text('+버튼 누르면 입력창을 띄웁니다.'),
+              const Text('해당 카드를 클릭하거나 오른쪽으로 밀면 수정,'),
+              const Text('왼쪽으로 밀면 삭제할 수 있습니다.'),
+              const SizedBox(height: 50,),
+              const Text('5. Theme 변경',style: TextStyle(fontSize: 19,fontWeight: FontWeight.w700),),
                 Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -120,27 +120,24 @@ class _TutorialState extends State<Tutorial> {
               Image.asset('images/darktheme.png',width: 150,height: 300,),
                 ],
               ),
-              SizedBox(height: 10,),
-              Text('해, 달 이모티콘을 클릭 시 테마 변경이 가능합니다.'),
-              SizedBox(height: 50,),
-              Text('6. 사진 권한',style: TextStyle(fontSize: 19,fontWeight: FontWeight.w700),),
+              const SizedBox(height: 10,),
+              const Text('해, 달 이모티콘을 클릭 시 테마 변경이 가능합니다.'),
+              const SizedBox(height: 50,),
+              const Text('6. 사진 권한',style: TextStyle(fontSize: 19,fontWeight: FontWeight.w700),),
                 Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
               Image.asset('images/photoaccess.png',width: 150,height: 300,),
                 ],
               ),
-              SizedBox(height: 10,),
-              Text('사진 권한이 없다는 창이 뜨면'),
-              Text('설정 → 개인정보 보호 및 보안 → 사진'),
-              Text('APP의 접근 권한을 바꿔주세요.'),
-              SizedBox(height: 50,),
-              Text('글씨체제작: 강원도교육청X혜움디자인',style: TextStyle(fontSize: 10),),
-              SizedBox(height: 70,),
-              
-              
-            ],
-              
+              const SizedBox(height: 10,),
+              const Text('사진 권한이 없다는 창이 뜨면'),
+              const Text('설정 → 개인정보 보호 및 보안 → 사진'),
+              const Text('APP의 접근 권한을 바꿔주세요.'),
+              const SizedBox(height: 50,),
+              const Text('글씨체제작: 강원도교육청X혜움디자인',style: TextStyle(fontSize: 10),),
+              const SizedBox(height: 70,),
+            ],  
           ),
         ),
       ),
